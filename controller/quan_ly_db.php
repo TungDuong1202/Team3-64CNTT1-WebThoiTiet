100ad14c7eca77d4825d6a8de5340f4b6f2926b8
TundDuong1202 <phanduong122004@gmail.com>
2023-10-10 21:08:39 +0700

    update controller

--- controller/quan_ly_db.php
+++ controller/quan_ly_db.php
@@ -16,7 +16,7 @@
         if(isset($_SESSION['role']) && $_SESSION['role'] == 1){
             require "../model/database.php";
     }?>
-            <button><a href="../view/giao_dien_admin.html">Thoát</a></button>
+            <a href="../view/giao_dien_admin.html">Thoát</a>
             <div class="tieude">
                 <h1>Danh sách dữ liệu người dùng</h1>
             </div>
@@ -30,54 +30,22 @@
                         <th>role</th>
                         <th>Chức năng</th>
                     </tr>
-                    <!-- <ul id="list-container">
-                        <li>                
-                            <tr>
-                                <td><input type="text" id="id"></td>
-                                <td><input type="text" id="username"></td>
-                                <td><input type="text" id="email"></td>
-                                <td><input type="text" id="password"></td>
-                                <td><input type="text" id="role"></td>
-                                <td><button class="nut1">Sửa</button><button class="nut2">Xóa</button></td>
-                            </tr>
-                        </li>
-                         <li>
-                            <tr>
-                                <td><input type="text" id="id"></td>
-                                <td><input type="text" id="username"></td>
-                                <td><input type="text" id="email"></td>
-                                <td><input type="text" id="password"></td>
-                                <td><input type="text" id="role"></td>
-                                <td><button class="nut1">Sửa</button><button class="nut2">Xóa</button></td>
-                            </tr>
-                        </li>
-                        <li>
-                            <tr>
-                                <td><input type="text" id="id"></td>
-                                <td><input type="text" id="username"></td>
-                                <td><input type="text" id="email"></td>
-                                <td><input type="text" id="password"></td>
-                                <td><input type="text" id="role"></td>
-                                <td><button class="nut1">Sửa</button><button class="nut2">Xóa</button></td>
-                            </tr>
-                        </li> 
-                    </ul>-->
+                    <?php
+                        // $connection = mysqli_connect("$host", "$username", "$password", "$dbname");
+                        $sql = "SELECT * FROM users";
+                        $qr = mysqli_query($conn,$sql);
+                        while($rows = mysqli_fetch_array($qr)){
+                    ?>
+                    <tr>    
+                        <td><?php echo $rows["id"]; ?></td>
+                        <td><?php echo $rows["username"]; ?></td>
+                        <td><?php echo $rows["email"]; ?></td>
+                        <td><?php echo $rows["password"]; ?></td>
+                        <td><?php echo $rows["role"]; ?></td>
+                        <td><a href="sua.php?id=<?php echo $rows['id']?>">Sửa </a>| <a href="xoa.php?id=<?php echo $rows['id']?>"> Xóa</td>
+                    </tr>
+                    <?php } ?>
+                </table>
             </div>
-                <?php
-                    // $connection = mysqli_connect("$host", "$username", "$password", "$dbname");
-                    $sql = "SELECT * FROM users";
-                    $qr = mysqli_query($conn,$sql);
-                    while($rows = mysqli_fetch_array($qr)){
-                ?>
-                <!-- <tr>    
-                    <td><?php echo $rows["id"]; ?></td>
-                    <td><?php echo $rows["username"]; ?></td>
-                    <td><?php echo $rows["email"]; ?></td>
-                    <td><?php echo $rows["password"]; ?></td>
-                    <td><?php echo $rows["role"]; ?></td>
-                    <td><a href="sua.php?id=<?php echo $rows['id']?>">Sửa </a>| <a href="xoa.php?id=<?php echo $rows['id']?>"> Xóa</td>
-                </tr>
-                <?php } ?> -->
-            </table>
 </body>
 </html>
